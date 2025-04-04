25490e0de063c59504b1101580e3d80c34948161
npp2107 <142470779+npp2107@users.noreply.github.com>
2025-04-04 10:42:32 +0700

    config api gateway

--- profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
@@ -3,7 +3,6 @@ package npp.booksocialnetwork.profile.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import npp.booksocialnetwork.profile.dto.request.ProfileCreationRequest;
 import npp.booksocialnetwork.profile.dto.response.UserProfileResponse;
 import npp.booksocialnetwork.profile.service.UserProfileService;
 
@@ -13,8 +12,6 @@ import lombok.experimental.FieldDefaults;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,12 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Profile Controller", description = "APIs for Create, Get Profile")
 public class UserProfileController {
     UserProfileService userProfileService;
-
-    @Operation(summary = "Create Profile", description = "API Create Profile")
-    @PostMapping("/users")
-    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request) {
-        return userProfileService.createProfile(request);
-    }
 
     @Operation(summary = "Get Profile", description = "API Get Profile")
     @GetMapping("/users/{profileId}")
