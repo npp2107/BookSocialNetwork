6735a16ffbce44b18061d728db51fe79d6e08f5f
npp2107 <142470779+npp2107@users.noreply.github.com>
2025-04-04 10:30:05 +0700

    add api-gateway service

--- profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
@@ -12,6 +12,12 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
