dc04eb6ca5ca114bb38c30f2cfd6d0e8ee90ba27
npp2107 <142470779+npp2107@users.noreply.github.com>
2025-04-04 14:51:49 +0700

    add get all profiles to Profile service

--- profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/npp/booksocialnetwork/profile/controller/UserProfileController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -25,5 +27,10 @@ public class UserProfileController {
     @GetMapping("/users/{profileId}")
     UserProfileResponse getProfile(@PathVariable String profileId) {
         return userProfileService.getProfile(profileId);
+    }
+
+    @GetMapping("/users")
+    List<UserProfileResponse> getAllProfiles() {
+        return userProfileService.getAllProfiles();
     }
 }
