1296f35fd282a2a42baec656def5a1203b7f95f4
npp2107 <142470779+npp2107@users.noreply.github.com>
2025-04-04 22:46:51 +0700

    add request interceptor

--- identity-service/src/main/java/npp/booksocialnetwork/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/npp/booksocialnetwork/identity/repository/httpclient/ProfileClient.java
@@ -1,5 +1,6 @@
 package npp.booksocialnetwork.identity.repository.httpclient;
 
+import npp.booksocialnetwork.identity.configuration.AuthenticationRequestInterceptor;
 import npp.booksocialnetwork.identity.dto.request.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import npp.booksocialnetwork.identity.dto.request.ProfileCreationRequest;
 import npp.booksocialnetwork.identity.dto.response.UserProfileResponse;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(name = "profile-service", url = "${app.services.profile}",
+        configuration = { AuthenticationRequestInterceptor.class })
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     ApiResponse<UserProfileResponse> createProfile(@RequestBody ProfileCreationRequest request);
